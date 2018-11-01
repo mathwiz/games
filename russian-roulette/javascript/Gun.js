981a16a82ea7a73242ae131c315db9da45c42e52
mathwiz <mathwiz@gmail.com>
2018-11-01 14:42:33 -0400

    node module

---
+++ russian-roulette/javascript/Gun.js
@@ -0,0 +1,29 @@
+function Gun(shots) {
+    this.chambers = shots;
+    this.current = 1;
+    this.loaded = Math.floor(Math.random() * shots) + 1;
+
+    this.shoot = function () {
+        const fire = !this.isOut() && this.loaded == this.current;
+        this.current = this.isOut() ? this.current : this.current + 1
+        return fire;
+    }
+
+    this.getCurrent = function() {
+        return this.current
+    }
+
+    this.getChambers = function() {
+        return this.chambers
+    }
+
+    this.getLoaded = function() {
+        return this.loaded
+    }
+
+    this.isOut = function() {
+        return this.current == this.chambers
+    }
+}
+
+module.exports.Gun = Gun;
--- russian-roulette/javascript/Russian.js
+++ russian-roulette/javascript/Russian.js
@@ -1,36 +1,22 @@
-function Gun(shots) {
-    this.chambers = new Array(shots);
-    this.current = 0;
-    this.loaded = Math.floor(Math.random() * shots);
+const gun = require('./Gun');
+const g = new gun.Gun(6);
 
-    this.shoot = function () {
-        const fire = !this.isOut();
-        this.current = this.isOut() ? this.current : this.current + 1
-        return fire;
-    }
-
-    this.getCurrent = function() {
-        return this.current + 1
-    }
-
-    this.getChambers = function() {
-        return this.chambers.length
-    }
-
-    this.getLoaded = function() {
-        return this.loaded + 1
-    }
-
-    this.isOut = function() {
-        return this.current > this.getChambers() - 1
-    }
-}
-
-
-g = new Gun(6)
-console.log(g.getCurrent())
+console.log(g.isOut())
 console.log(g.getChambers())
 console.log(g.getLoaded())
-console.log(g.isOut())
+console.log(g.getCurrent())
 console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
+console.log(g.shoot())
+console.log(g.getCurrent())
 

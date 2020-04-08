3bd4d708d568b871cf078b95328c5e0bea6fbbde
Yohan Lee <yohanlee@yahoo.com>
2020-04-08 19:43:09 -0400

    chess

--- chess/binary.lisp
+++ chess/binary.lisp
@@ -15,15 +15,23 @@
 
 
 (defun int->bits (int)
-  (labels ((bit (index)
+  (labels ((pad (lst)
+             (let ((len (- 64 (length lst))))
+               (append (make-list len :initial-element 0) lst)))
+           (get-bit (index)
              (if (logbitp index int)
                  1
                  0))
            (recur (index acc)
-             (cond ((= index 0) (reverse acc))
-                   (t (recur (1- index) (cons (bit (1- index)) acc))))))
+             (cond ((= index 0) (pad (reverse acc)))
+                   (t (recur (1- index) (cons (get-bit (1- index)) acc))))))
     (recur (integer-length int) nil)))
 
 
 (defun bits->board (bits)
-  nil)
+  (labels ((recur (bs row board)
+             (cond ((null bs) (reverse (cons (reverse row) board)))
+                   (t (if (= (length row) 8)
+                          (recur (cdr bs) (list (car bs)) (cons (reverse row) board))
+                          (recur (cdr bs) (cons (car bs) row) board))))))
+    (recur bits nil nil)))

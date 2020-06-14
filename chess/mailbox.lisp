ad06f80f3f0c8db863d177f670935ca428cf9168
Yohan Lee <yohanlee@yahoo.com>
2020-06-14 14:59:37 -0400

    chess

--- chess/mailbox.lisp
+++ chess/mailbox.lisp
@@ -1,7 +1,30 @@
 (setf *pos-min* 22)
 (setf *pos-max* 98)
 (setf *off-board* 100)
+(setf *empty* 0)
 
+(setf *bk* -1)
+(setf *bq* -2)
+(setf *br* -3)
+(setf *bn* -4)
+(setf *bb* -5)
+(setf *bp* -6)
+
+(setf *wk* 1)
+(setf *wq* 2)
+(setf *wr* 3)
+(setf *wn* 4)
+(setf *wb* 5)
+(setf *wp* 6)
+
+
+(setf *white-pieces* (make-hash-table :test #'equal))
+(setf *black-pieces* (make-hash-table :test #'equal))
 
 (defun on-board (pos)
   (and (>= pos *pos-min*) (<= pos *pos-max*)))
+
+
+(defun init-pieces ()
+  nil)
+

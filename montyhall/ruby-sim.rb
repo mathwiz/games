4137bebb389af16956556c0e8a48926bbbc8855a
Yohan Lee <yohanlee@yahoo.com>
2020-08-13 11:51:34 -0400

    ruby

--- montyhall/ruby-sim.rb
+++ montyhall/ruby-sim.rb
@@ -24,12 +24,26 @@ class SampleSpaceWithReplacement
     def size
         @data.size
     end
-end
+end #SampleSpaceWithReplacement
 
 
-ss = SampleSpaceWithReplacement.new [ 1, 2, 3 ]
+class SampleSpaceWithoutReplacement < SampleSpaceWithReplacement
+    def next
+        if @data.empty?
+            raise "no values exist in the sample space"
+        end
+        @data.delete_at(rand @data.size)
+    end
+end #SampleSpaceWithoutReplacement
+
+
+# ss = SampleSpaceWithReplacement.new [ 1, 2, 3 ]
 # 5.times { puts ss.next }
 # 5.times { puts ss.nextBatch(4).to_s }
+
+ss = SampleSpaceWithoutReplacement.new [ 1, 2, 3 ]
+# 5.times { puts ss.next }
+# 2.times { puts ss.nextBatch(2).to_s }
 
 puts '* Monty Hall Simulation'
 

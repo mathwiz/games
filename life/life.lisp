397a602afdb1de1d39246913dbb7875f79fff8bf
Yohan Lee <yohanlee@yahoo.com>
2021-02-08 14:58:37 -0500

    life

--- life/life.lisp
+++ life/life.lisp
@@ -1,17 +1,31 @@
+(defparameter *LIVE* "*")
+(defparameter *DEAD* ".")
+
 (defun life ()
-  (let ((_SIZE nil) 
+  (let ((_SIZE nil)
         (_GEN1 nil)
         (_GEN2 nil))
-    (progn 
+    (progn
       (init)
       (main-loop))))
 
 
 (defun init ()
-  nil)
+  (format t "Size of Life: ")
+  (progn
+    (setf _SIZE (read))
+    (setf _GEN1 (make-array (* _SIZE _SIZE)))
+    (setf _GEN2 (make-array (* _SIZE _SIZE)))))
+
 
 (defun main-loop ()
-  (princ 'done))
+  (let ((cmd (read)))
+    (princ cmd))
+  (show))
+
+
+(defun show ()
+  (princ _GEN1))
 
 ;; Run it
 (life)

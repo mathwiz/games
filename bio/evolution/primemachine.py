36494c91570881ac8cffcba52973f5c213229384
Yohan Lee <yohanlee@yahoo.com>
2022-12-15 14:05:22 -0500

    machine

--- bio/evolution/primemachine.py
+++ bio/evolution/primemachine.py
@@ -1,15 +1,13 @@
 def move(code):
   nyb = code - 1
-  return -1 if (nyb == 0 or nyb == 1) else 1
+  return -1 if (nyb == 0 or nyb == 2) else 1
 
 def write(code):
   nyb = code - 1 
-  return 0 if (nyb == 0 or nyb == 2) else 1
+  return 0 if (nyb == 0 or nyb == 1) else 1
 
 def state(a, b):
-  na = a - 1
-  nb = b - 1
-  return 
+  return ((a-1)*4) + (b-1)
 
 primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,
           53,59,61,67,71,73,79,83,89,97]
@@ -25,30 +23,26 @@ def fitness(tape):
   return correct
       
 class Machine:
-  CardWidth = 6
-  MoveMax = 1000
-
   def __init__(self, sequence):
     self.program = sequence
     self.tape_length = 100
+    self.tape_pos = 0
+    self.tape = [0 for i in range(self.tape_length)]
+    self.card_width = 6
+    self.card = 0
+    self.moves = 0
 
   def step(self):
-    tape = [0 for i in range(self.tape_length)]
-    pos = 0
-    counter = 0
-    moves = 0
-    code = self.program[counter]
-    for i in range(Machine.MoveMax):
-      moves = i + 1
-
-      counter += 1
-    return (tape, moves)
+    current_sym = self.tape[self.tape_pos]
+    code_start = self.card * self.card_width
+    code_end = code_start + self.card_width
+    code = self.program[code_start:code_end]
+    index = current_sym * (self.card_width/2)
+    self.tape[self.tape_pos] = write(code[index])
+    self.tape_pos = move(code[0])
 
   def exec(self):
-    tape = [0 for i in range(Machine.TapeLength)]
-    pos = 0
-    counter = 0
-    moves = 0
+    move_max = 1000
     code = self.program[counter]
     for i in range(Machine.MoveMax):
       moves = i + 1
--- bio/evolution/test_primemachine.py
+++ bio/evolution/test_primemachine.py
@@ -11,10 +11,41 @@ C[0] = 1
 C[99] = 1
 
 class MyTestCase(unittest.TestCase):
+  def test_write(self):
+    self.assertEqual(write(1), 0)
+    self.assertEqual(write(2), 0)
+    self.assertEqual(write(3), 1)
+    self.assertEqual(write(4), 1)
+
+  def test_move(self):
+    self.assertEqual(move(1), -1)
+    self.assertEqual(move(2), 1)
+    self.assertEqual(move(3), -1)
+    self.assertEqual(move(4), 1)
+
   def test_fitness(self):
     self.assertEqual(fitness(A), 75)
     self.assertEqual(fitness(B), 78)
     self.assertEqual(fitness(C), 73)
+
+  def test_state(self):
+    self.assertEqual(state(1,1), 0)
+    self.assertEqual(state(2,1), 4)
+    self.assertEqual(state(3,1), 8)
+    self.assertEqual(state(4,1), 12)
+    self.assertEqual(state(1,2), 1)
+    self.assertEqual(state(2,2), 5)
+    self.assertEqual(state(3,2), 9)
+    self.assertEqual(state(4,2), 13)
+    self.assertEqual(state(1,3), 2)
+    self.assertEqual(state(2,3), 6)
+    self.assertEqual(state(3,3), 10)
+    self.assertEqual(state(4,3), 14)
+    self.assertEqual(state(1,4), 3)
+    self.assertEqual(state(2,4), 7)
+    self.assertEqual(state(3,4), 11)
+    self.assertEqual(state(4,4), 15)
+
 
 if __name__ == '__main__':
     unittest.main()

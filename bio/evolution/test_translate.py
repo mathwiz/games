59239de9fa88b9fbdc0a9930001b9db5d87454b5
Yohan Lee <yohanlee@yahoo.com>
2023-12-31 11:55:42 -0500

    dna

---
+++ bio/evolution/test_translate.py
@@ -0,0 +1,8 @@
+from translate import *
+
+seq1 = 'TCTGCTTTAACTTAT'
+seq2 = 'AGTGCGCTGACCTAC'
+
+print("Seq1: ", seq1, "Translated: ", translate(seq1))
+print("Seq2: ", seq2, "Translated: ", translate(seq2))
+

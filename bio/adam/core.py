41154c10c9d4a4240e406f5b3fd684a741d95d6b
Yohan Lee <yohanlee@yahoo.com>
2024-03-23 11:04:16 -0400

    python

--- bio/adam/core.py
+++ bio/adam/core.py
@@ -1,2 +1,17 @@
 import numpy as np
 
+
+def unique_elements(xs):
+  unique = {}
+  for x in xs:
+    if x in unique:
+      unique[x] = unique[x] + 1
+    else:
+      unique[x] = 1
+  return [key for key in unique if unique[key] == 1]
+
+
+unique = unique_elements([1,2,3,4,4,5,6,6,7,8,8,8,8])
+
+print(unique)
+

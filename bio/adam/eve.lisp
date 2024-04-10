870f936226d859f1ec5c20428e4e3d9bc9e70d0d
Yohan Lee <yohanlee@yahoo.com>
2024-04-10 08:38:36 -0400

    eve

--- bio/adam/eve.lisp
+++ bio/adam/eve.lisp
@@ -1,3 +1,26 @@
+;; Helpers *************************************************
+
+(defun get-unique-elements (lst)
+  (labels ((recur (l seen acc)
+             (cond ((null l) acc)
+                   ((or (member (car l) (cdr l))
+                        (member (car l) seen))
+                    (recur (remove-if (lambda (x) (equal x (car l))) l) (cons (car l) seen) acc))
+                   (t
+                    (recur (cdr l) (cons (car l) seen) (cons (car l) acc))))))
+    (recur lst nil nil)))
+
+
+(defun get-unique-elements-hash (lst)
+  (let ((hash-table (make-hash-table :test 'equal)))
+    (dolist (element lst)
+      (incf (gethash element hash-table 0)))
+    (loop for key being the hash-keys of hash-table
+          if (= (gethash key hash-table) 1)
+          collect key)))
+
+;; Helpers *************************************************
+
 (defconstant FEMALE 0)
 (defconstant MALE 1)
 
@@ -52,9 +75,8 @@
 (defun count-elements (lst selector)
   (length (remove-if-not selector lst)))
 
-(count-unique nil)
-
-(length (remove-if (lambda (x) (= x 0)) (loop for x from 0 to 1000 collect (new-gender))))
+;; tester
+;;(length (remove-if (lambda (x) (= x 0)) (loop for x from 0 to 1000 collect (new-gender))))
  
 (defun reproduce-pop (females males)
   (labels ((recur (fs ms acc)
@@ -96,24 +118,4 @@
         (counter 0))
     (simulate pop gens))
 )
-
-
-(defun get-unique-elements (lst)
-  (labels ((recur (l seen acc)
-             (cond ((null l) acc)
-                   ((or (member (car l) (cdr l))
-                        (member (car l) seen))
-                    (recur (remove-if (lambda (x) (equal x (car l))) l) (cons (car l) seen) acc))
-                   (t
-                    (recur (cdr l) (cons (car l) seen) (cons (car l) acc))))))
-    (recur lst nil nil)))
-
-
-(defun get-unique-elements-hash (lst)
-  (let ((hash-table (make-hash-table :test 'equal)))
-    (dolist (element lst)
-      (incf (gethash element hash-table 0)))
-    (loop for key being the hash-keys of hash-table
-          if (= (gethash key hash-table) 1)
-          collect key)))
 

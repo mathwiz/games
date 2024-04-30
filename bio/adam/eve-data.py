30a38aeda3dcd5317cce92bf151d07b3bc8ba1ba
Yohan Lee <yohanlee@yahoo.com>
2024-04-30 07:54:10 -0400

    eve

--- bio/adam/eve-data.py
+++ bio/adam/eve-data.py
@@ -1,5 +1,16 @@
 import csv
 
+def process_row(row):
+  pass
+
+def extract_rep(rows):
+  pass
+
+def process_rep(rows):
+  final_pop = 0
+  last_gen = 1
+  return final_pop, last_gen
+
 infile = "a.out"
 datafile = "eve.dat"
 
--- bio/adam/eve-run.lisp
+++ bio/adam/eve-run.lisp
@@ -4,5 +4,8 @@
 (let ((reps 3)
       (pop 20)
       (gens 5))
-(loop repeat reps do
-      (run-sim pop gens)))
+
+(dotimes (i reps)
+      (progn
+        (format t "~&~A" (1+ i))
+        (run-sim pop gens))))

e966615718a1ff18442349c18b8dc9b5090db23b
Yohan Lee <yohanlee@yahoo.com>
2025-10-14 12:47:05 -0400

    penrose

--- penrose-machines/testlib.lisp
+++ penrose-machines/testlib.lisp
@@ -4,8 +4,9 @@
            	(pass   (concatenate 'string "passed: " (write-to-string val)))
            	(fail   (concatenate 'string "failed: " (write-to-string val)))
            	(retval (cond 
-                     	((stringp expected) (string= val expected))
-	                   	(t (= val expected))))
+                     	((stringp expected) (string= (write-to-string val) expected))
+                     	((stringp val) (string= val (write-to-string expected)))
+	                   	(t (eq val expected))))
            )
      	(format t "~%~A  ~A"
                   (if retval pass fail)

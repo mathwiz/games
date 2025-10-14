fde4ed48caa5ac2e5f571b2b807c7c31362a899f
Yohan Lee <yohanlee@yahoo.com>
2025-10-14 16:35:47 -0400

    penrose

--- penrose-machines/test-binary.lisp
+++ penrose-machines/test-binary.lisp
@@ -1,9 +1,9 @@
 (load "testlib.lisp")
 (load "binary.lisp")
 
-(check-expect '(list 1 0 1 1) (int-to-bin 11))
-(check-expect '(list 1 1 0 0) (int-to-bin 12))
-(check-expect '(list 1 1 0 1) (int-to-bin 13))
+(check-expect '(int-to-bin 11) (list 1 0 1 1))
+(check-expect '(int-to-bin 12) (list 1 1 0 0))
+(check-expect '(int-to-bin 13) (list 1 1 0 1))
 
-(check-expect "1100" (bin-to-string(int-to-bin 12)))
-(check-expect "1101" (bin-to-string(int-to-bin 13)))
+(check-expect '(bin-to-string(int-to-bin 12)) "1100")
+(check-expect '(bin-to-string(int-to-bin 13)) "1101")

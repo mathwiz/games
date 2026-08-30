40da263d38bd01e8ba8905bfbdca7c9658953e9b
Yohan Lee <mathwiz@gmail.com>
2026-08-30 11:28:05 -0400

    seq_along

--- bio/genetic_algos/ga.R
+++ bio/genetic_algos/ga.R
@@ -1,2 +1,14 @@
 # setwd('Github/games/bio/genetic_algo')
-population <- matrix(1:6700, nrow=100, ncol=67)
+population <- matrix(1:320, nrow=10, ncol=32)
+
+pop <- list()
+
+pop[[1]] <- rep(0, 10)
+
+for (i in 1:10) { pop[[i]] <- rep(0, 16) }
+
+nums <- 1:10
+nums2 <- numeric(length(nums))
+
+for (i in seq_along(nums)) { nums2[i] <- sqrt(nums[i]) }
+
